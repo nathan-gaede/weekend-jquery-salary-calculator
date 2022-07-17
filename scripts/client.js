990a4004b650ec30ffccfa94c29e7933300825cb
Nathan Gaede <nathangaede@Nathans-MacBook-Pro.local>
2022-07-16 19:36:03 -0500

    Created addEmployeeInputs function to collect employee data and append to table. Tested whether first name is added to the table.

--- scripts/client.js
+++ scripts/client.js
@@ -3,6 +3,25 @@ console.log('scripts sourced')
 $(document).ready(readyNow); 
 
 function readyNow() {
-    console.log('testing readyNow')
+    console.log('testing readyNow');
+    $('#employee-submit').on('click', addEmployeeInputs);
 }
+
+function addEmployeeInputs () {
+    console.log('in addEmployeeInputs');
+    let firstName = $('#first-name-input').val();
+    let lastName = $('#last-name-input').val();
+    let employeeId = $('#id-input').val();
+    let employeeTitle = $('#title-input').val();
+    let annualSalary = $('#annual-salary-input').val();
+    console.log(firstName,lastName,employeeId,employeeTitle,annualSalary);
+    $('#employee-data').append(`
+    <tr>
+        <td>${firstName}</td>
+    </tr>
+    `)
+}
+// function clearInputFields () {
+//     console.log('in clearInputFields')
+// }
 
